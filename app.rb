bcb737033e9ff2b024f0714e4bd5d5fb95646302
Rigel St. Pierre <rigel@bandsintown.com>
2014-10-02 12:43:37 -0700

    adds giphy gem

--- app.rb
+++ app.rb
@@ -6,4 +6,9 @@ GIPHY_KEY="dc6zaTOxFJmzC" # Public Beta Key
   get "/" do
     erb :index
   end
+
+  post "/gif_search" do
+    search= params[:search]
+    Giphy.search(search, {limit: 50, offset: 25})
+  end
 end
